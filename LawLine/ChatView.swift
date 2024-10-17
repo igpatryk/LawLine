4f8059630720ebe90a07dc57f6526910380c67e4
igpatryk <igpatryk7@gmail.com>
2024-10-17 12:25:21 +0200

    First steps with Gemini

--- LawLine/ChatView.swift
+++ LawLine/ChatView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import GoogleGenerativeAI
 
 struct Message: Identifiable {
     let id = UUID()
@@ -7,6 +8,8 @@ struct Message: Identifiable {
 }
 
 struct ChatView: View {
+    let model = GenerativeModel(name: "gemini-1.5-flash", apiKey: APIKey.default)
+    @State var aiResponse = ""
     @State private var messages: [Message] = [
         Message(text: "W czym mogę Ci pomóc?", isUserMessage: false)
     ]
@@ -48,14 +51,14 @@ struct ChatView: View {
     }
     
     func sendMessage() {
-        // Dodanie wiadomości użytkownika
+        // Add user message
         let userMessage = Message(text: userInput, isUserMessage: true)
         messages.append(userMessage)
         
-        // Wyczyszczenie pola tekstowego
+        // Clear the text field
         userInput = ""
 
-        // Wywołanie API i dodanie odpowiedzi
+        // Call API and add response
         fetchAIResponse(for: userMessage.text) { response in
             DispatchQueue.main.async {
                 let aiMessage = Message(text: response, isUserMessage: false)
@@ -63,15 +66,28 @@ struct ChatView: View {
             }
         }
     }
+
     
     // Funkcja do integracji z API Gemini
     func fetchAIResponse(for userMessage: String, completion: @escaping (String) -> Void) {
-        // Tutaj musisz wywołać swoje API i przekazać odpowiedź do completion
-        // Zamiast przykładowej odpowiedzi poniżej wywołaj swoje API Gemini
-        let sampleResponse = "To jest przykładowa odpowiedź od AI."
-        completion(sampleResponse)
+        Task {
+            do {
+                let response = try await model.generateContent(userMessage)
+                
+                guard let text = response.text else  {
+                    completion("Sorry, I could not process that.\nPlease try again.")
+                    return
+                }
+                
+                completion(text) // Call completion with the fetched text
+                
+            } catch {
+                completion("Something went wrong!\n\(error.localizedDescription)")
+            }
+        }
     }
-}
+
+    }
 
 // Widok dymku wiadomości
 struct MessageBubble: View {
